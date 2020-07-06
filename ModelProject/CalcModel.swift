9df1dbb73c6238fb29902b0973ca907c44b6dd1c
Yuta Fujii <yutayutakid21@gmail.com>
2020-07-06 18:00:26 +0900

    課題

---
+++ ModelProject/CalcModel.swift
@@ -0,0 +1,19 @@
+//
+//  CalcModel.swift
+//  ModelProject
+//
+//  Created by Yuta Fujii on 2020/07/06.
+//  Copyright © 2020 Yuta Fujii. All rights reserved.
+//
+
+import Foundation
+
+class CalcModel{
+    
+    var number1 = Int()
+    var number2 = Int()
+    
+    
+    
+}
+
--- ModelProject/ViewController.swift
+++ ModelProject/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+   
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    @IBOutlet weak var numberTextField2: UITextField!
+  
+    /*
+     
+     課題:このリポジトリからフォークさせ、自分のリモートリポジトリからローカルへ落とし、作成されたModelクラス(CakcModel)から、
+     足し算、引き算、掛け算、割り算を行う関数を作成し、
+     自分のリモートリポジトリのmasterへpushしてください。
+     その後私のリモートリポジトリへPull Requestを送ってください。
+     */
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func result(_ sender: Any) {
+        
+        
+    }
+    
 }
 
